11e8f7b8807ec682a8ad9568af295f30d6e76d87
NRProjects <56905917+NRProjects@users.noreply.github.com>
2023-10-06 17:40:56 -0700

    Added SMP command

--- src/main/java/plugins/nate/smp/commands/SMPCommand.java
+++ src/main/java/plugins/nate/smp/commands/SMPCommand.java
@@ -3,10 +3,31 @@ package plugins.nate.smp.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static plugins.nate.smp.utils.ChatUtils.coloredChat;
 
 public class SMPCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(coloredChat("&cOnly players can use this command!"));
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            player.sendMessage(coloredChat(
+                """
+                &8&m------------------------&8&l[&a&lSMP&8&l]&8&m------------------------
+                &aWelcome to the CoolmentSMP! This server is driven by public and private plugins to help give the user the best experience possible. The SMP plugin is main driver of this server and is meant to provide a vanilla-esque experience. With additions in and out of game including QoL additions and discord integration. If you have any feature requests or issues contact staff on our discord. Thank you and have fun!
+                &8&m------------------------&8&l[&a&lSMP&8&l]&8&m------------------------
+                """
+            ));
+            System.out.println("Debug: Sending message to player");
+        } else {
+            System.out.println("Debug: No 'smp' argument provided");
+        }
         return true;
     }
 }
--- src/main/java/plugins/nate/smp/utils/CommandRegistration.java
+++ src/main/java/plugins/nate/smp/utils/CommandRegistration.java
@@ -3,10 +3,13 @@ package plugins.nate.smp.utils;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import plugins.nate.smp.SMP;
+import plugins.nate.smp.commands.ClaimCommand;
+import plugins.nate.smp.commands.SMPCommand;
 
 public class CommandRegistration {
     public static void registerCommands(SMP plugin) {
 //        setupCommand("claim", new ClaimCommand(plugin.getClaimSelectionManager()), plugin);
+        setupCommand("smp", new SMPCommand(), plugin);
     }
 
     private static void setupCommand(String commandLabel, CommandExecutor executor, SMP plugin) {
