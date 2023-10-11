28b25969abc98df854c4f1174030b3c74c336f6c
NRProjects <56905917+NRProjects@users.noreply.github.com>
2023-10-10 17:07:18 -0700

    Limited veinminer enchant to pickaxes only

--- src/main/java/plugins/nate/smp/listeners/enchantments/EnchantmentListener.java
+++ src/main/java/plugins/nate/smp/listeners/enchantments/EnchantmentListener.java
@@ -1,6 +1,7 @@
 package plugins.nate.smp.listeners.enchantments;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
@@ -15,11 +16,12 @@ public class EnchantmentListener implements Listener {
 
     @EventHandler
     public static void onEnchantItem(EnchantItemEvent event) {
+        ItemStack item = event.getItem();
+        Material material = item.getType();
 
-        if (event.getExpLevelCost() >= 25) {
+        if (event.getExpLevelCost() >= 25 && isPickaxe(material)) {
             double chance = Math.random();
             if (chance > 0.05) {
-                ItemStack item = event.getItem();
                 ItemMeta meta = item.getItemMeta();
 
                 List<String> lore = meta.hasLore() ? meta.getLore() : new ArrayList<>();
@@ -30,5 +32,14 @@ public class EnchantmentListener implements Listener {
                 item.setItemMeta(meta);
             }
         }
+    }
+
+    private static boolean isPickaxe(Material material) {
+        return material == Material.WOODEN_PICKAXE ||
+                material == Material.STONE_PICKAXE ||
+                material == Material.IRON_PICKAXE ||
+                material == Material.GOLDEN_PICKAXE ||
+                material == Material.DIAMOND_PICKAXE ||
+                material == Material.NETHERITE_PICKAXE;
     }
 }
