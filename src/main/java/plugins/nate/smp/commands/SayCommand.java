d7b4c3d845975ef920c5f3c9fbac62a9762fff58
Tyler Rife <tylerrifeasd@gmail.com>
2023-10-20 00:50:05 -0600

    Add a permission to /say and also join the message easier

--- src/main/java/plugins/nate/smp/commands/SayCommand.java
+++ src/main/java/plugins/nate/smp/commands/SayCommand.java
@@ -9,20 +9,19 @@ import org.bukkit.command.ConsoleCommandSender;
 import plugins.nate.smp.utils.ChatUtils;
 
 public class SayCommand implements CommandExecutor {
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("smp.say")) {
+            sender.sendMessage(ChatUtils.coloredChat(ChatUtils.PREFIX + ChatUtils.DENIED_COMMAND));
+            return true;
+        }
+
         if (args.length == 0) {
             sender.sendMessage(ChatColor.RED + "Usage: /say <message>");
             return true;
         }
 
-        StringBuilder message = new StringBuilder();
-        for (String arg : args) {
-            message.append(arg).append(" ");
-        }
-
-        String formattedMessage = message.toString().trim();
+        String formattedMessage = String.join(" ", args);
         if (sender instanceof ConsoleCommandSender) {
             formattedMessage = ChatUtils.coloredChat("&8&l[&c&lSERVER&8&l] &c" + formattedMessage);
         }
