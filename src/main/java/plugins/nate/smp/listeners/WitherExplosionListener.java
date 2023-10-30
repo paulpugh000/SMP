5fe7a7d06112493da0addc68feaca9630884f85d
Tyler Rife <tylerrifeasd@gmail.com>
2023-10-30 15:55:05 -0600

    Check each individual block location

--- src/main/java/plugins/nate/smp/listeners/WitherExplosionListener.java
+++ src/main/java/plugins/nate/smp/listeners/WitherExplosionListener.java
@@ -1,5 +1,7 @@
 package plugins.nate.smp.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,13 +9,19 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 import plugins.nate.smp.SMP;
 import plugins.nate.smp.utils.SMPUtils;
 
+import java.util.List;
+import java.util.Objects;
+
 public class WitherExplosionListener implements Listener {
     @EventHandler
     public void onWitherExplosion(EntityExplodeEvent event) {
         if (event.getEntityType() == EntityType.WITHER || event.getEntityType() == EntityType.WITHER_SKULL) {
-            if (!SMPUtils.isFlagAllowedAtLocation(SMP.WITHER_EXPLOSIONS, event.getLocation())) {
-                event.setCancelled(true);
-            }
+            List<Block> toRemove = event.blockList().stream()
+                    .filter(Objects::nonNull)
+                    .filter(block -> !SMPUtils.isFlagAllowedAtLocation(SMP.WITHER_EXPLOSIONS, block.getLocation()))
+                    .toList();
+
+            event.blockList().removeAll(toRemove);
         }
     }
 }
