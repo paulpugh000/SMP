d941a777fd8564deb8ed4c77036326354f9ade7e
NRProjects <56905917+NRProjects@users.noreply.github.com>
2023-12-03 18:27:48 -0800

    Fixed anvil issues regarding all custom enchantments.

--- src/main/java/plugins/nate/smp/managers/EnchantmentManager.java
+++ src/main/java/plugins/nate/smp/managers/EnchantmentManager.java
@@ -11,19 +11,26 @@ import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 import plugins.nate.smp.enchantments.CustomEnchant;
 import plugins.nate.smp.enchantments.TimberEnchant;
 import plugins.nate.smp.enchantments.VeinMinerEnchant;
+import plugins.nate.smp.utils.SMPUtils;
 
 import java.lang.reflect.Field;
 import java.util.*;
 
 public class EnchantmentManager implements Listener {
     public static final Map<String, Enchantment> ENCHANTMENTS = new HashMap<>();
+    public static final Map<Enchantment, String> LORE_MAP = new HashMap<>();
+
 
     static {
         ENCHANTMENTS.put("vein_miner", new VeinMinerEnchant());
         ENCHANTMENTS.put("timber", new TimberEnchant());
+
+        LORE_MAP.put(new VeinMinerEnchant(), ChatColor.GOLD + "Vein Miner");
+        LORE_MAP.put(new TimberEnchant(), ChatColor.GOLD + "Timber");
     }
 
     public static void registerEnchantment(String key) {
@@ -91,35 +98,86 @@ public class EnchantmentManager implements Listener {
             return;
         }
 
+        if (firstItem.getType() == Material.ENCHANTED_BOOK) {
+            return;
+        }
+
+        if (hasCustomEnchant(firstItem) && hasCustomEnchant(secondItem) && firstItem.getType() != secondItem.getType()) {
+            return;
+        }
+
+        Map<Enchantment, Integer> mergedEnchantments = mergeEnchantments(firstItem.getEnchantments(), secondItem.getEnchantments());
+
+        ItemStack resultItem = event.getResult();
+        if (resultItem == null || hasCustomEnchant(secondItem)) {
+            resultItem = firstItem.clone();
+        }
+
+        ItemMeta resultMeta = resultItem.getItemMeta();
+
         if (secondItem.getType() == Material.ENCHANTED_BOOK) {
             EnchantmentStorageMeta bookMeta = (EnchantmentStorageMeta) secondItem.getItemMeta();
-
-            ENCHANTMENTS.values().stream()
-                    .filter(enchantment -> enchantment instanceof CustomEnchant)
-                    .filter(enchantment -> enchantment.canEnchantItem(firstItem))
-                    .filter(bookMeta::hasStoredEnchant)
-                    .findFirst()
-                    .ifPresent(enchantment -> {
-                        ItemMeta toolMeta = firstItem.getItemMeta();
-
-                        String enchantLore = ((CustomEnchant) enchantment).getLore();
-
-                        if (toolMeta.hasLore() && toolMeta.getLore().contains(enchantLore)) {
-                            return;
-                        }
-
-                        ItemStack resultItem = firstItem.clone();
-                        ItemMeta resultMeta = resultItem.getItemMeta();
-
-                        List<String> lore = resultMeta.hasLore() ? resultMeta.getLore() : new ArrayList<>();
-                        lore.add(enchantLore);
-                        resultMeta.setLore(lore);
-
-                        resultItem.setItemMeta(resultMeta);
-                        resultItem.addUnsafeEnchantment(enchantment, 1);
-
-                        event.setResult(resultItem);
-                    });
+            if (bookMeta != null && bookMeta.hasStoredEnchants()) {
+                mergedEnchantments = mergeEnchantments(mergedEnchantments, bookMeta.getStoredEnchants());
+            }
         }
+
+        mergedEnchantments.forEach((enchantment, level) -> resultMeta.addEnchant(enchantment, level, true));
+
+        List<String> resultLore = getCustomLore(mergedEnchantments);
+        resultMeta.setLore(resultLore);
+        resultItem.setItemMeta(resultMeta);
+        
+        event.setResult(resultItem);
+    }
+
+    private static boolean hasCustomEnchant(@NotNull ItemStack item) {
+        if (item.getItemMeta() == null) {
+            return false;
+        }
+
+        for (Map.Entry<Enchantment, Integer> entry : item.getEnchantments().entrySet()) {
+            Enchantment enchantment = entry.getKey();
+            String enchantmentKey = enchantment.getKey().getKey();
+
+            if (ENCHANTMENTS.containsKey(enchantmentKey)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static Map<Enchantment, Integer> mergeEnchantments(Map<Enchantment, Integer> firstItemEnchants, Map<Enchantment, Integer> secondItemEnchants) {
+        Map<Enchantment, Integer> mergedEnchantments = new HashMap<>(firstItemEnchants);
+
+        secondItemEnchants.forEach((enchantment, level) -> {
+            if (mergedEnchantments.containsKey(enchantment)) {
+                int existingLevel = mergedEnchantments.get(enchantment);
+                if (existingLevel == level) {
+                    mergedEnchantments.put(enchantment, Math.min(existingLevel + 1, enchantment.getMaxLevel()));
+                } else {
+                    mergedEnchantments.put(enchantment, Math.max(existingLevel, level));
+                }
+            } else {
+                mergedEnchantments.put(enchantment, level);
+            }
+        });
+
+        return mergedEnchantments;
+    }
+
+    private static List<String> getCustomLore(Map<Enchantment, Integer> enchantments) {
+        List<String> customLore = new ArrayList<>();
+
+        for (Enchantment enchantment : enchantments.keySet()) {
+            if (LORE_MAP.containsKey(enchantment)) {
+                customLore.add(LORE_MAP.get(enchantment));
+            }
+        }
+
+        return customLore;
     }
 }
+
+
