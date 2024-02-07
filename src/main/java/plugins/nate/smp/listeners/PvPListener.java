c782c93ef58095527e46bdced51854519488a4f9
Paul Pugh <paulpugh000@gmail.com>
2024-02-07 13:10:51 -0800

    Moved effect location

--- src/main/java/plugins/nate/smp/listeners/PvPListener.java
+++ src/main/java/plugins/nate/smp/listeners/PvPListener.java
@@ -7,13 +7,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import plugins.nate.smp.managers.PlayerSettingsManager;
 import plugins.nate.smp.managers.PvPManager;
 import plugins.nate.smp.utils.ChatUtils;
 
-import java.util.HashMap;
-import java.util.UUID;
 
 public class PvPListener implements Listener {
     @EventHandler
@@ -30,21 +27,21 @@ public class PvPListener implements Listener {
 
         if (!damagerPvPEnabled) {
             ChatUtils.sendMessage(pvpDamager, ChatUtils.PREFIX + "&7You have PvP disabled.");
-            disabledEffect(pvpDamager, pvpReceiver);
+            disabledEffect(pvpDamager, pvpDamager.getLocation());
             event.setCancelled(true);
             return;
         }
         if (!receiverPvPEnabled) {
             ChatUtils.sendMessage(pvpDamager, ChatUtils.PREFIX + "&c" + pvpReceiver.getName() + " &7has PvP disabled.");
-            disabledEffect(pvpDamager, pvpReceiver);
+            disabledEffect(pvpDamager, pvpReceiver.getLocation());
             event.setCancelled(true);
             return;
         }
         PvPManager.lastDmgTime.put(pvpDamager.getUniqueId(), System.currentTimeMillis());
     }
 
-    private static void disabledEffect(Player damager, Player receiver) {
-        damager.spawnParticle(Particle.VILLAGER_ANGRY, receiver.getLocation(), 3, .2f, .2f, .2f);
-        damager.playSound(receiver.getLocation(), Sound.BLOCK_NOTE_BLOCK_DIDGERIDOO, 0.5f, 0f);
+    private static void disabledEffect(Player damager, Location location) {
+        damager.spawnParticle(Particle.VILLAGER_ANGRY, location, 3, .2f, .2f, .2f);
+        damager.playSound(location, Sound.BLOCK_NOTE_BLOCK_DIDGERIDOO, 0.5f, 0f);
     }
 }
